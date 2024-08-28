6dde4832979d49780fdaeda75cbd0c07a3f0763f
Lawson Woods <ljwoods2@asu.edu>
2024-08-27 23:49:52 -0700

    shell config

--- lammps/test_lammps.py
+++ lammps/test_lammps.py
@@ -4,39 +4,38 @@ import pytest
 import subprocess
 
 def run_sim_and_wait(command, match_string):
-    """Block until simulation is ready to accept connections.
-    Yields the output directory for validation.
-    Requires that the simulation engine is added to the PATH."""
-    with tmpdir.as_cwd():
-        with open("gmx_output.log", "w") as f:
-            p = subprocess.Popen(
-                command,
-                stdin=subprocess.PIPE,
-                stdout=f,
-                stderr=f,
-                text=True,
-                bufsize=1,
-            )
-            try:
-                yield port
-            finally:
-                # Terminate the process
-                p.terminate()
-                try:
-                    p.wait(timeout=10)
-                except subprocess.TimeoutExpired:
-                    p.kill()
-                    p.wait()
 
-                # Ensure all file descriptors are closed
-                f.close()
+
+    p = subprocess.Popen(
+        command,
+        stdin=subprocess.PIPE,
+        stdout=f,
+        stderr=f,
+        text=True,
+        bufsize=1,
+    )
+    return p
+        
 
 
 @pytest.fixture(scope="function")
-def simulation():
+def simulation(tmp_path):
+    """Block until simulation is ready to accept connections.
+    Yields the output directory for validation.
+    Requires that the simulation engine is added to the PATH."""
     command = []
     match_string = "IMD: Will wait until I have a connection and IMD_GO orders."
-    run_sim_and_wait(command, match_string)
+    with tmp_path.as_cwd():
+        p = run_sim_and_wait(command, match_string)
+        yield tmp_path
+
+        # Terminate the process
+        p.terminate()
+        try:
+            p.wait(timeout=10)
+        except subprocess.TimeoutExpired:
+            p.kill()
+            p.wait()
+
 
 class TestLammpsIMDv3:
-    
